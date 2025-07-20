e79bbe59087aaf13ba1a041e459327c050768348
NeilFG <nflordel@my.centennialcollege.ca>
2025-07-20 17:53:39 -0400

    Connected the database, expanded on  the models
    Fully connected to the mongoDB database, used env to hide password.
    
    Completed the product model
    
    expanded on the product controller.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,28 @@ import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Initialize the Express application
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+//database 
+connectDB();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
 
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/auth", authRoutes);
 
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log('Server is running on PORT:', PORT);
 });
