a54fa38badc5f816e89d910a56bd002e9b911f74
Roland Oliverr <rolandpetrola11@gmail.com>
2025-08-12 10:40:10 -0400

    put config.js

---
+++ frontend/src/config.js
@@ -0,0 +1,7 @@
+// src/config.js
+export const API_BASE =
+  import.meta.env.MODE === 'production'
+    ? import.meta.env.VITE_API_URL            // this matches your Vercel var name
+    : (import.meta.env.VITE_API_URL ?? 'http://localhost:3000');
+
+console.log('API_BASE =', API_BASE);
